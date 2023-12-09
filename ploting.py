19920accfd71105ffc51694ef63fc82bb01e656e
stefanpeknik <stefa9982@seznam.cz>
2023-12-09 23:55:25 +0100

    l

--- ploting.py
+++ ploting.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import re as re
+import re
 import sys
 
 CAR_MAX_VELOCITY = 38
@@ -14,44 +14,52 @@ def velocity_to_color(velocity):
 
 def get_data(file="data.txt"):
     with open(file) as f:
-        line = f.readline()
-        while re.search("[a-zA-Z]", line) is not None:
-            line = f.readline()
+        # Use a more efficient way to skip lines with letters
+        for line in f:
+            if not re.search("[a-zA-Z]", line):
+                break
         return [l.strip().split("|") for l in f.readlines()]
 
 
-source = sys.argv[1]
-data = get_data(source)
-
-# Convert lines to a 2D array
-road_state = np.zeros((len(data), len(data[0])))
-for i, line in enumerate(data):
-    for j, char in enumerate(line):
-        if char.isdigit():
-            road_state[i, j] = int(char)
+def create_road_state(data):
+    # Use list comprehension for a more concise and potentially faster approach
+    return np.array(
+        [[int(char) if char.isdigit() else 0 for char in line] for line in data]
+    )
 
 
-# Create a colorful plot
-fig, ax = plt.subplots()
+def plot_road_state(road_state, file_name):
+    # Use numpy functions to find indices where road_state is greater than 0
+    indices = np.argwhere(road_state > 0)
 
-for i in range(len(data)):
-    for j in range(len(data[0])):
-        if road_state[i, j] > 0:
-            color = velocity_to_color(road_state[i, j])
-            ax.scatter(i, j, color=color, s=2)
+    # Create a colorful plot using a single scatter call
+    colors = [velocity_to_color(road_state[i, j]) for i, j in indices]
+    plt.scatter(indices[:, 1], indices[:, 0], color=colors, s=1)
 
-# Customize plot appearance
-ax.set_xlabel("Time")
-ax.set_ylabel("Distance on the Road")
-ax.set_title("Road State Over Time")
-ax.set_aspect("equal")
-ax.grid(True)
+    # Customize plot appearance
+    plt.xlabel("Distance on the Road")
+    plt.ylabel("Time")
+    plt.title("Road State Over Time")
+    plt.gca().set_aspect("equal")
+    plt.grid(True)
 
-# Specify the file name based on the variable name
-file_name = "plot_{}.png".format(source)
+    # Save the plot as a PNG file
+    plt.savefig(file_name)
 
-# Save the plot as a PNG file
-fig.savefig(file_name)
+    # Show the plot
+    # plt.show()
 
-# Show the plot
-# plt.show()
+
+def main():
+    source = sys.argv[1]
+    data = get_data(source)
+    road_state = create_road_state(data)
+
+    # Specify the file name based on the variable name
+    file_name = "plot_{}.png".format(source)
+
+    plot_road_state(road_state, file_name)
+
+
+if __name__ == "__main__":
+    main()
