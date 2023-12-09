a8488557e94ee4727554abbf023b30ba45c5a0ad
stefanpeknik <stefa9982@seznam.cz>
2023-12-09 04:16:19 +0100

    more comms

--- src/Car.cpp
+++ src/Car.cpp
@@ -2,17 +2,21 @@
 #include <iostream>
 
 void Car::updateVelocity(int whereIam, int spaceInFront) {
+  // step 1/ acceleration
   this->velocity += this->a;
   if (this->velocity > this->V_max) {
     this->velocity = this->V_max;
   }
+  // step 2/ validating space in front
   if (this->velocity + sg() > spaceInFront) {
     this->velocity =
         std::min(this->velocity, (int)std::ceil(spaceInFront - sg() / 2));
   }
+  // step 3/ random slow down
   if (std::rand() % 100 < this->chanceToSlowDown) {
     this->velocity -= this->a;
   }
+  // step 4/ ensuring non-negative velocity
   if (this->velocity < 0) {
     this->velocity = 0;
   }
