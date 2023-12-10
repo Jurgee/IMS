faec3b86cd05ef66e212fbdce633bf9ec4701028
stefanpeknik <stefa9982@seznam.cz>
2023-12-10 20:13:23 +0100

    edit sim

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -5,28 +5,11 @@ void Simulation::run() {
   std::vector<std::shared_ptr<Car>> lane =
       std::vector<std::shared_ptr<Car>>(parameters.roadLength);
 
-  // initialize the cars in the lane
-  // if there are more cars than the road can hold, the rest will be ignored
-  int count = 0;
-  for (int i = 0; count < parameters.carCount && i < parameters.roadLength;
-       i++) {
-    // the first fifth of the cars will start at max velocity and are scattered
-    // evenly with save gaps
-    if (count < parameters.carCount / 3) {
-      Car car(parameters.carMaxVelocity, parameters.acceleration,
-              parameters.carMaxVelocity, parameters.levelOfAggressiveness,
-              parameters.chanceToSlowDown);
-      lane[i] = std::make_shared<Car>(car);
-      count++;
-      i += car.getVelocity() + car.sg();
-    }
-    // the rest will start as stuck at 0 in traffic jam
-    else {
-      Car car(0, parameters.acceleration, parameters.carMaxVelocity,
-              parameters.levelOfAggressiveness, parameters.chanceToSlowDown);
-      lane[i] = std::make_shared<Car>(car);
-      count++;
-    }
+  // initialize the cars
+  for (int i = 0; i < parameters.carCount; i++) {
+    Car car(0, parameters.acceleration, parameters.carMaxVelocity,
+            parameters.levelOfAggressiveness, parameters.chanceToSlowDown);
+    lane[i] = std::make_shared<Car>(car);
   }
 
   // initialize the road with the lane
